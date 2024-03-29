cdf5c637798ceb45d4f49feb31a0eed754d6613b
webgod0123 <miracletalent0123@gmail.com>
2024-03-29 18:01:20 +0530

    Initialization 💿

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import Navbar from "@/components/Navbar";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(inter.className, "bg-white rounded-md")}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
